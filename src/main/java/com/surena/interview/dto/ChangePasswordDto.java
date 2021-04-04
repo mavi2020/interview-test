97d5940535d92de6421a66f16ec62463119ea108
Mousavi <mavi.mousavi@gmail.com>
2021-04-04 09:50:53 +0430

    Add validation

--- src/main/java/com/surena/interview/dto/ChangePasswordDto.java
+++ src/main/java/com/surena/interview/dto/ChangePasswordDto.java
@@ -1,12 +1,23 @@
 package com.surena.interview.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class ChangePasswordDto {
+    @NotBlank(message = "نام کاربری نمی تواند خالی باشد")
+    @ApiModelProperty(required = true, example = "mavi")
     private String username;
+
+    @NotBlank(message = "رمز عبور قبلی نمی تواند خالی باشد")
+    @ApiModelProperty(required = true, example = "123")
     private String oldPassword;
+
+    @NotBlank(message = "رمز عبور جدید نمی تواند خالی باشد")
+    @ApiModelProperty(required = true, example = "123@45")
     private String newPassword;
 }
--- src/main/java/com/surena/interview/dto/UserDto.java
+++ src/main/java/com/surena/interview/dto/UserDto.java
@@ -1,18 +1,33 @@
 package com.surena.interview.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 @Getter
 @Setter
 public class UserDto {
     private long id;
+
+    @NotBlank(message = "نام کاربری نمی تواند خالی باشد")
+    @ApiModelProperty(required = true, example = "mavi")
     private String username;
+
+    @NotBlank(message = "رمز عبور نمی تواند خالی باشد")
+    @ApiModelProperty(required = true, example = "123@45")
     private String password;
+
+    @NotBlank(message = "نام نمی تواند خالی باشد")
+    @ApiModelProperty(required = true, example = "shima")
     private String firstName;
+
+    @NotBlank(message = "نام خانوادگی نمی تواند خالی باشد")
+    @ApiModelProperty(required = true, example = "mousavi")
     private String lastName;
+
     private Date createDate;
     private Date modifiedDate;
 }
